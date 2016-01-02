1c769f19d4c90fabc237f8b0854e71aa584fdc2b
Mark Katona <katona.mark@gmail.com>
2016-01-02 13:50:55 +0000

    First few things added
    //pos //set

---
+++ SNEditPositionGet.cs
@@ -0,0 +1,107 @@
+﻿using SharedGameData;
+using SNScript;
+using System;
+using System.Linq;
+
+namespace ScriptsExample
+{
+    class SNEditPositionGet : GameCommand
+    {
+        public override string[] Aliases
+        {
+            get { return new string[] { "//pos" }; }
+        }
+
+        public override string CommandDescription
+        {
+            get
+            {
+                return "A test command in ScriptsExample.";
+            }
+        }
+
+        public override Priviledges Priviledge
+        {
+            get { return Priviledges.Player; }
+        }
+        
+        public SNEditPositionGet(IGameServer server)
+            : base(server)
+        {
+        }
+
+        public override bool Use(IActor actor, string message, string[] parameters)
+        {
+            //Store if user wants pos 1 or 2
+            int _ID = new int();
+
+            try
+            {
+                _ID = Int32.Parse(parameters[1]);
+            } catch(FormatException e)
+            {
+                Server.ChatManager.SendActorMessage("Parameter could not be parsed.", actor);
+                return false;
+            }
+            
+            if(2 < _ID || _ID < 0)
+            {
+                Server.ChatManager.SendActorMessage("Use parameter 1 or 2. You used: " + _ID , actor);
+                return false;
+            }
+            //Get systems
+            var SystemsCollection = Server.Biomes.GetSystems();
+
+            //Get system player is in
+            uint currentSystemID = actor.InstanceID;
+
+            //Define currentSystems for TryGetValue
+            IBiomeSystem currentSystem;
+
+            //Find the currentSystem based on its ID
+            SystemsCollection.TryGetValue(currentSystemID, out currentSystem);
+
+            //Get the chunk's ID that the player is in
+            uint currentChunkID = actor.ConnectedChunk;
+
+            //Search current system for the chunk based on its ID
+            IChunk currentChunk = currentSystem.ChunkCollection.First(item => item.ID == currentChunkID);
+
+            //Allign player with local chunk grid
+            Point3D actorPos = new Point3D((int)actor.LocalChunkTransform.X, (int)actor.LocalChunkTransform.Y, (int)actor.LocalChunkTransform.Z);
+
+            //Convert local Point to Sector Point
+            Point3D globalPos = new Point3D((int)currentChunk.Position.X + actorPos.X - 32 /2, (int)currentChunk.Position.Y + actorPos.Y - 32 /2, (int)currentChunk.Position.Z + actorPos.Z - 32/2);
+
+            //Return the saved data for testing
+            Point3D returnSave = new Point3D();
+
+            switch (_ID)
+            {
+                case 1:
+                    //Push position to Player's Session storage for Pos1
+                    actor.SessionVariables.Add("SNEditPos1", (object)globalPos);
+                    returnSave = (Point3D)actor.SessionVariables["SNEditPos1"];
+                    break;
+                case 2:
+                    //Push position to Player's Session storage for Pos2
+                    actor.SessionVariables.Add("SNEditPos2", (object)globalPos);
+                    returnSave = (Point3D)actor.SessionVariables["SNEditPos2"];
+                    break;
+            }
+
+            //Return this to the player
+            Server.ChatManager.SendActorMessage("Pos" + _ID + " set.", actor);
+            //Testing only
+            Server.ChatManager.SendActorMessage("Global Pos: X=" + globalPos.X.ToString() + " Y=" + globalPos.Y.ToString() + " Z=" + globalPos.Z.ToString(), actor);
+            Server.ChatManager.SendActorMessage("Actor Pos: X=" + actorPos.X.ToString() + " Y=" + actorPos.Y.ToString() + " Z=" + actorPos.Z.ToString(), actor);
+            Server.ChatManager.SendActorMessage("Chunk Pos: X=" + ((int)currentChunk.Position.X).ToString() + " Y=" + ((int)currentChunk.Position.Y).ToString() + " Z=" + ((int)currentChunk.Position.Z).ToString(), actor);
+            //Return Saved Data
+            Server.ChatManager.SendActorMessage("Stored:" + returnSave.X.ToString() + " Y=" + returnSave.Y.ToString() + " Z=" + returnSave.Z.ToString(), actor);
+
+
+            //Command executed successfully 
+            return true;
+        }
+    }
+}
---
+++ SNEditSet.cs
@@ -0,0 +1,111 @@
+﻿using SharedGameData;
+using SNScript;
+using GameServer;
+using System;
+using System.Linq;
+
+namespace ScriptsExample
+{
+    class SNEditSet : GameCommand
+    {
+        public override string[] Aliases
+        {
+            get { return new string[] { "//set" }; }
+        }
+
+        public override string CommandDescription
+        {
+            get
+            {
+                return "A test command in ScriptsExample.";
+            }
+        }
+
+        public override Priviledges Priviledge
+        {
+            get { return Priviledges.Player; }
+        }
+
+        public SNEditSet(IGameServer server) : base(server)
+        {
+        }
+
+        public override bool Use(IActor actor, string message, string[] parameters)
+        {
+            
+            //Only takes 2 arguments //set and blockID
+            if(parameters.Length > 2)
+            {
+                Server.ChatManager.SendMessage("Too many arguments.", actor);
+                return false;
+            }
+
+            //Check if entered blockID is valid TODO
+            if(true)
+            {
+                ushort blockID = parameters[1];
+            } else
+            {
+                Server.ChatManager.SendMessage("Invalid block entered.", actor);
+                return false;
+            }
+
+            //Varible Point3D
+            Point3D dynamic = new Point3D();
+
+            //2 positions for what the user set
+            Point3D pos1 = new Point3D();
+            Point3D pos2 = new Point3D();
+
+            //List of blocks need to be plased
+            List<Point3D> locationList = new List<Point3D>();
+
+            //Maybe add this into a class later?
+            if(!checkPositions(actor, out pos1, out pos2))
+            {
+                return false;
+            }
+
+            //Get the difference between the two position
+            dynamic = pos1 - pos2;
+
+            //Get every block on X
+            while(dynamic.x != 0)
+            {
+                Point3D temp = new Point3D();
+                temp = pos1;
+                temp.x = pos1 + dynamic.x;
+
+                //Get every block on Y
+                while(dynamic.y != 0)
+                {
+                    temp.y = dynamic.y;
+
+                    //Get every block on Z
+                    while(dynamic.z != 0)
+                    {
+                        temp.z = dynamic.z;
+                        locationList.add(temp);
+
+                        //Get dynamic closer to 0
+                        if (dynamic.z > 0) { dynamic.z++; } else { dynamic.z--; }
+                    }
+
+                    //Get dynamic closer to 0
+                    if (dynamic.y > 0) { dynamic.y++; } else { dynamic.y--; }
+                }
+                //Get dynamic closer to 0
+                if (dynamic.x > 0) { dynamic.x++; } else { dynamic.x--; }
+
+            }
+
+            //TODO
+            IChunk currentChunk = getChunkFromGlobal(pos1, actor);
+
+            //Change blocks
+            currentChunk.ChangeBlockBunch(locationList, blockID);
+
+            return true;
+        }
+    }
+}
